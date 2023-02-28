b246f3da191f509e7b7f7ec6ccf12c28310d664d
Michael-Maina <michaelmaina02@gmail.com>
2023-02-28 10:24:30 +0300

    Renamed variable

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,8 +19,8 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 
 	new_node->n = value;
 	new_node->parent = parent;
-	new->left = NULL;
-	new->right = NULL;
+	new_node->left = NULL;
+	new_node->right = NULL;
 
 	return (new_node);
 }
